46f33bdf88b9f8fa77f2246d5bc9b272e45c128c
majo <maria.riofrio.mr@gmail.com>
2020-08-03 20:55:06 -0400

    fixed navbar

--- myportfolio/src/component/Navbar/index.js
+++ myportfolio/src/component/Navbar/index.js
@@ -1,35 +1,47 @@
 
 import React from "react";
-import { link, useLocation} from "react-router-dom";
+import { Link} from "react-router-dom";
+import "../Navbar/style.css";
 
 
 function NavTabs() {
-    
-    const location = useLocation();
-    
-    return (
-        <>
-    <ul className="nav nav-tabs" bg="info" variant="dark">
-        <li className="nav-item">
-        <a to="/" className = {location.pathname ==="/" ? "nav-link active" : "nav-link"}>
-            Maria Jose
-        </a>
-        </li>
-        <li className="nav-item">
-        <a to="/Portfolio" className = {location.pathname ==="/Portfolio" ? "nav-link active" : "nav-link"}>
-            Portfolio
-        </a>
-        </li>
-        <li className="nav-item">
-        <a to="/Contact" className = {location.pathname ==="/Contact" ? "nav-link active" : "nav-link"}>
-            Contact Info
-        </a>
-        </li>
-        
-      </ul>
-      </>
 
-        );
-    }
-    
-    export default NavTabs;
+    return (
+        <nav className="navbar navbar-expand-lg  navbar-dark">
+            <div className="newFlex">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link
+                            to="/"
+                            className={
+                                window.location.pathname === "/" || window.location.pathname === "/Profilecard" ? "nav-link active" : "nav-link"}
+                        >
+                            Home
+            </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to="/Portfolio"
+                            className={
+                                window.location.pathname === "/Portfolio" ? "nav-link active" : "nav-link"}
+                        >
+                            Portfolio
+      </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to="/Contact"
+                            className={
+                                window.location.pathname ===  "/Contact" ? "nav-link active" : "nav-link"}
+                        >
+                            Contact Info
+       </Link>
+                    </li>
+                    </ul>
+     </div>
+     </nav>
+
+    );
+}
+
+export default NavTabs;
